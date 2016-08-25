774424eecb619dac688bbf87d7bc9982df384303
Tedman Hsu <twh26@cornell.edu>
2016-08-25 21:41:05 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  def intialize(name, job)
+    @name = name
+    @job = job
+  end
+  def name= (name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def job= (job)
+    @job = job
+  end
+  def job
+    @job
+  end
+  
+end
