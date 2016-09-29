51364119cd919e015769ffbe09a99faa5da9b229
Christiane Heiligers <tina.heiligers@gmail.com>
2016-09-29 22:28:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  # attr_accessor(:name, :breed)
+
+  # def initialize(name,breed)
+  #   @name = name
+  #   @breed = breed
+  # end
+
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  # attr_accessor(:name, :job)
+  # def initialize(name, job)
+  #   @name = name
+  #   @job = job
+  # end
+  def name
+    @name
+  end
+  def name=(name)
+    @name = name
+  end
+  def job
+    @job
+  end
+  def job=(job)
+    @job = job
+  end
+end
