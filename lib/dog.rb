903594f086f3e9b893cdd9308c535b0af602e1d7
Edward Goo <edwardx89@yahoo.com>
2016-10-04 01:44:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name= new_name
+    @name = new_name
+  end
+  def name
+    @name
+  end
+  def breed= breed_name
+    @breed = breed_name
+  end
+  def breed
+    @breed
+  end
+
+end
