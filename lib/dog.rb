7c985b47677908d0297561080ff177201d097f98
Muhammad BIMONARETGA <bimonaretga@outlook.com>
2016-10-28 05:19:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name
+    @name
+  end
+  def name=(name_the_dog)
+    @name = (name_the_dog)
+  end
+  def breed
+    @breed
+  end
+  def breed=(what_breed)
+    @breed = (what_breed)
+  end
+end
