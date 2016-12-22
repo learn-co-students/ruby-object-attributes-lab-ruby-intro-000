29d4f4b56902f2acd7c101e42c14e4b74d83f970
Salvatore McCarty <salvatore.mccarty@gmail.com>
2016-12-22 18:32:58 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+  def name                        #get name of Dog
+    @name
+  end
+  def name=(name)             #set name of Dog
+    @name = name
+  def breed                       #get breed of Dog
+    @breed
+  end
+  def breed=(breed)           #set breed of Dog
+    @breed = breed
+  end
+
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name                    #get name of Person
+    @name
+  end
+  def name=(name)             #set name of Person
+    @name = name
+  end
+  def job                     #get job of Person
+    @job
+  end
+  def job=(job)               #set job of Person
+    @job = job
+  end
+end
