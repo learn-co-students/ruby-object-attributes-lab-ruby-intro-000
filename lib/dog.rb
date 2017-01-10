c9b876b4b087942dfbae933378717d32cc137d2f
Terry Blue <terryblue@comcast.net>
2017-01-10 02:04:32 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,39 @@
+class Dog
+
+    def name=(new_name)
+      @name = new_name
+    end
+
+    def name
+      @name
+    end
+
+    def breed=(new_name)
+      @breed = new_name
+    end
+
+    def breed
+      @breed
+    end
+
+end
+
+class Person
+
+    def name=(new_name)
+      @name = new_name
+    end
+
+    def name
+      @name
+    end
+
+    def job=(new_name)
+      @job = new_name
+    end
+
+    def job
+      @job
+    end
+
+end
