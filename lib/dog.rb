e9f395bef67552bb2f827e712da686e4f73ba744
Terry Blue <terryblue@comcast.net>
2017-01-10 02:12:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,39 +1,19 @@
 class Dog
 
-    def name=(new_name)
-      @name = new_name
+    def name=(name)
+      @name = name
     end
 
     def name
       @name
     end
 
-    def breed=(new_name)
-      @breed = new_name
+    def breed=(breed)
+      @breed = breed
     end
 
     def breed
       @breed
-    end
-
-end
-
-class Person
-
-    def name=(new_name)
-      @name = new_name
-    end
-
-    def name
-      @name
-    end
-
-    def job=(new_name)
-      @job = new_name
-    end
-
-    def job
-      @job
     end
 
 end
