f2aaed1dfc0d0a6e1341751e5401af0327a05312
Adam Ergas <adamergas@yahoo.com>
2017-02-08 16:42:18 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,17 @@
 class Dog
+  #set method for dog name
   def name= (name)
     @name = name
   end
+  #get method for dog name
   def name
     @name
   end
+  #set method for dog breed
   def breed= (breed)
     @breed = breed
   end
+  #get method for dog breed
   def breed
     @breed
   end
--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,17 @@
 class Person
+  #set method for person name
   def name= (name)
     @name = name
   end
+  #get method for person name
   def name 
     @name
   end
+  #set method for person job
   def job= (job)
     @job = job
   end
+  #get method for person job
   def job
     @job
   end
