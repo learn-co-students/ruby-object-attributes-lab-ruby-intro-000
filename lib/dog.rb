b029075604db1d67008925a68bb63c46b767318f
Mary Benitez <meebenitez@gmail.com>
2017-05-03 03:01:18 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name=(get_name)
+    @name = get_name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(get_breed)
+    @breed = get_breed
+  end
+
+  def breed
+    @breed
+  end
+
+end
