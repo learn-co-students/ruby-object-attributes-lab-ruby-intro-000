883e3f7303cabe3ae0bcaa4d35e01a09bd65eedf
Kari Schendel <firefaded@yahoo.com>
2017-08-05 02:52:26 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
