24b7a242ed97909885c413444fe5df2531d09eb8
Lora Woodford <lorawoodford@gmail.com>
2017-09-01 20:41:25 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,14 @@
 class Person
+  def name=(name)
+    @name = name
+  end
   def name
     @name
   end
-  def name=(new_name)
-    @name = new_name
+  def job=(job)
+    @job = job
   end
   def job
     @job
-  end
-  def job=(new_job)
-    @job = new_job
   end
 end
