1315508e44a69f5d686adecdb256a44f3aa95a7d
Justin Phillips <justinphil02@yahoo.com>
2017-09-06 23:11:16 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+@name
+@breed
+def name=(string)
+  @name = string
+end
+def name
+  @name
+end
+def breed=(string)
+  @breed = string
+end
+def breed
+  @breed
+end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+@name
+@job
+def name=(name)
+  @name = name
+end
+def name
+  @name
+end
+def job=(string)
+  @job = string
+end
+def job
+  @job
+end
+
+end
