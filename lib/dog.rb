35504382cdac0e445c339329009fc2db77c6b2eb
Tony Ho <ho.tonym@gmail.com>
2018-04-26 03:00:48 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name=(new_doggy_name)
+    @name = new_doggy_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_doggy_breed)
+    @breed = new_doggy_breed
+  end
+end
