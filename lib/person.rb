3c332918ae366a2e80038949638a8ac0e03dcf08
Sky Resendez <sky.resendez@gmail.com>
2018-09-03 23:13:55 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+  def initialize
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(nname)
+    @name = nname
+  end
+  def job(job)
+    @job = job
+  end
+  def job=(njob)
+    @job = njob
+  end
+  def job
+    @job
+  end
+end
