e0cc2c8ac0a20335fc2c3d51910c92278e40922c
shivani brijmohan <sbrij001@yahoo.com>
2018-11-23 19:58:57 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+#use setter and getter methods to give a person a name and a job
+
+class Person
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def job
+    @job
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+
+end
