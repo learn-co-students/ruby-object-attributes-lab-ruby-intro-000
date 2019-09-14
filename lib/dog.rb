c3750d2a89cc9882e1a259490f9a9e4e3d75e806
Shawn Huang Fernandes <sfernandes2014@gmail.com>
2019-09-14 19:09:20 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name= (dogName)
+    @name = dogName
+  end
+
+  def name
+    @name
+  end
+
+  def breed= (dogBreed)
+    @breed = dogBreed
+  end
+
+  def breed
+    @breed
+  end
+end
