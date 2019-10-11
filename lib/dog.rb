b949229c3460c918c4d7ee2cda8a6919d5b2589c
Aaron Gokani <aarongokani@hotmail.com>
2019-10-11 09:09:39 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  def initialize
+    @name = name
+    @breed = breed
+  end
+  def name
+    puts @name
+  end
+
+
+  def name = (new_name)
+    @name = new_name
+  end
+
+  def breed
+    puts @breed
+  end
+
+  def breed = (new_breed)
+    @breed = new_breed
+  end
+
+end
