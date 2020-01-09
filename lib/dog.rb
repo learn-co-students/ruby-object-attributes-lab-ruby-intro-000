0a138dd13ad82aea9a232b7d9575b743550fadc1
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-09 12:51:29 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog 
+def initialize(name) 
+ '#name' = @name 
+end 
+
+def name = (name)
+  @name 
+end 
+  def initialize(breed)
+    '#breed' = @breed
+  end
+
+  def breed
+    @breed
+  end
+end
+
