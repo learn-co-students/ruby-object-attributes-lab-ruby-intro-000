25dca4bede6aa0d5ce2e606e1f02241cece8d92a
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-09 13:51:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,14 @@
 class Dog 
-def initialize(name) 
- '#name' = @name 
-end 
-
-def name = (name)
-  @name 
-end 
-  def initialize(breed)
-    '#breed' = @breed
+  def name=(dog_name) 
+  @name = dog_name  
+  end
+  
+  def name 
+     @name 
+  end 
+  
+  def breed=(breed)
+    @breed = breed
   end
 
   def breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
  
-  def initialize(name)
+  def name=(name)
     @name = name
   end
  
@@ -8,10 +8,12 @@ class Person
     @name
   end
  
-  def name=(new_name)
-    @name = new_name
+  def job=(job)
+    @job = job
   end
- 
+  def job
+    @job
+  end 
 end
 
 
