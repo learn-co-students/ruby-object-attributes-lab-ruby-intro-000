3710d68eb67cc6cbf1c51387a18da017da280f27
Sam Bryant <bryantsj@gmail.com>
2020-02-01 00:41:26 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name=(human_name)
+    @name = human_name
+  end
+  def name
+    @name
+  end
+  def job=(human_job)
+    @job = human_job
+  end
+  def job
+    @job
+  end
+end
