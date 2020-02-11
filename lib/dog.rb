04709e34da1a170b7a472ac03ab39926d9e54b7c
dazzling-acrobat-7297 <abhips@gmail.com>
2020-02-11 20:37:56 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+
+  def breed
+      @breed
+  end
+
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def name
+    @name
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def job=(job_name)
+    @job = job_name
+  end
+
+  def job
+    @job
+  end
+
+  def name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+end
