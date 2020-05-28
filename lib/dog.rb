e73db081249cdad074889fa33b88740d26a0e208
Jasmine Lok <jasminellok@gmail.com>
2020-05-28 23:44:55 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def name
+    @name
+  end
+
+  def name=(update_name)
+    @name = update_name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(update_breed)
+    @breed= update_breed
+  end 
+
+end
