34263e747cd6b477227b1f14bea75964aa71aeab
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-06-30 18:03:16 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  # #name= method to set the instance variable @name
+  def name=(name)
+    @name = name
+  end
+  # #name method to get @name
+  def name
+    @name
+  end
+  
+  # #breed= method to set the variable @breed
+  def breed=(breed)
+    @breed = breed
+  end
+  # #breed method to get @breed
+  def breed
+    @breed
+  end
+end
