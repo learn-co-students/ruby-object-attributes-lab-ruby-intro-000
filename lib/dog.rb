40ecebf769693486bf694b17e95a13d6f63ae61f
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-20 15:54:50 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+class Dog
+  def initiliaze(name, breed)
+    @name = name
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def breed=(breed_name)
+    @breed = breed_name
+  end
+
+end
