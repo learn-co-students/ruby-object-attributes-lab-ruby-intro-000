c84052f45c0ed40dc5366b6a69de4bd04cd1c002
DavidRMorphew <morphewd@umich.edu>
2020-09-04 13:12:17 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(job_of_person)
+        @job = job_of_person
+    end
+
+    def job
+        @job
+    end
+end
