79df27f6e7b22e9c34e8bfac243d2840ae8fdcc3
Andre Powers <andre@stuypend.com>
2021-02-11 20:32:34 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+
+  def name= (new_name)
+    @name = new_name
+  end
+
+  def name
+    return @name
+  end
+
+  def breed= (brd)
+    @breed = brd
+  end
+
+  def breed
+    return @breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name= (nom)
+    @name = nom
+  end
+
+  def name
+    return @name
+  end
+
+  def job= (jub)
+    @job = jub
+  end
+
+  def job
+    return @job
+  end
+
+end
